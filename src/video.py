ff6d9840c37c93b57d3bb3fcb40c4c2319aa8d5d
Denis Prusakov <Denikpr@gmail.com>
2023-09-07 22:35:50 +0300

    homework4 src.video

---
+++ src/video.py
@@ -0,0 +1,12 @@
+from src.channel import Channel
+
+
+class Video:
+    def __init__(self, video_id):
+        self.video_id = video_id
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
