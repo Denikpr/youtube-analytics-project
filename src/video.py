f1b758cf470b46ffbc7311452d59b3f6d37f284f
Denis Prusakov <Denikpr@gmail.com>
2023-09-19 21:59:11 +0300

    homework6

--- src/video.py
+++ src/video.py
@@ -6,15 +6,23 @@ from googleapiclient.discovery import build
 class Video:
     def __init__(self, video_id):
         self.video_id = video_id
-        self.response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                       id=video_id).execute()
-
-        self.title = self.response['items'][0]['snippet']['title']
-        self.description = self.response['items'][0]['snippet']['description']
-        self.likeCount = int(self.response['items'][0]['statistics']['likeCount'])
-        self.viewCount = int(self.response['items'][0]['statistics']['viewCount'])
-        self.duration = self.response["items"][0]['contentDetails']['duration']
-        self.url_video = f"https://youtu.be/{self.video_id}"
+        try:
+            self.response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=video_id).execute()
+            self.title = self.response['items'][0]['snippet']['title']
+        except IndexError:
+            self.response = None
+            self.title = None
+            self.url_video = None
+            self.like_count = None
+            self.viewCount = None
+            self.duration = None
+        else:
+            self.description = self.response['items'][0]['snippet']['description']
+            self.like_count = int(self.response['items'][0]['statistics']['likeCount'])
+            self.viewCount = int(self.response['items'][0]['statistics']['viewCount'])
+            self.duration = self.response["items"][0]['contentDetails']['duration']
+            self.url_video = f"https://youtu.be/{self.video_id}"
 
     @classmethod
     def get_service(cls):
